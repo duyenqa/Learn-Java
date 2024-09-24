dfc77aa642a3612c3c1dc0f83c1a342811054842
duyenqa <ttemp8399@gmail.com>
2024-09-24 17:27:05 +0700

    Create unit10.java

---
+++ unit10.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        try{
+            System.out.println("Executing try block code");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
